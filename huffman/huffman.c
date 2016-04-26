1fcd69047cb25766f57b0a0074d795b939e116cb
Narongsak Mala <narongsm@ais.co.th>
2016-04-26 15:47:14 +0700

    Revised huffman

--- huffman/huffman.c
+++ huffman/huffman.c
@@ -3,256 +3,13 @@
 #include "huffman.h"
 #include "tables.h"
 
-
-#define STRCMP(s1, s2) strcmp(s1, s2) == 0
-#define STRCASECMP(s1, s2) strcasecmp(s1, s2) == 0
-#define STRCPY(s1,s2) strcpy(s1,s2)
-#define STRLEN(s1) (s1 == NULL)?0:strlen(s1)
-#define STRRESET(s1) (s1[0] = 0)
-#define pair(n,v) {n,v}
-
-// DONOT edit sequence of enum
-enum static_table_enum{
-	IDX_AUTHORITY		= 0x1,
-	IDX_METHOD_GET		= 0x2,
-	IDX_METHOD_POST		= 0x3,
-	IDX_PATH			= 0x4,
-	IDX_PATH_HTML		= 0x5,
-	IDX_SCHEME_HTTP		= 0x6,
-	IDX_SCHEME_HTTPS	= 0x7,
-	IDX_STATUS_200		= 0x8,
-	IEX_STATUS_204		= 0x9,
-	IDX_STATUS_206		= 0xa,
-	IDX_STATUS_304		= 0xb,
-	IDX_STATUS_400		= 0xc,
-	IDX_STATUS_404		= 0xd,
-	IDX_STATUS_500		= 0xe,
-	IDX_ACCEPT_CHARSET	= 0xf,
-	IDX_ACCEPT_ENCODING = 0x10,
-	IDX_ACCEPT_LANGUAGE = 0x11,
-	IDX_ACCEPT_RANGES	= 0x12,
-	IDX_ACCEPT			= 0x13,
-	IDX_ACCESS_CONTROL_ALLOW_ORIGIN = 0x14,
-	IDX_AGE				= 0x15,
-	IDX_ALLOW			= 0x16,
-	IDX_AUTHORIZATION	= 0x17,
-	IDX_CACHE_CONTROL	= 0x18,
-	IDX_CONTENT_DISPOSITION = 0x19,
-	IDX_CONTENT_ENCODING	= 0x1a,
-	IDX_CONTENT_LANGUAGE	= 0x1b,
-	IDX_CONTENT_LENGTH		= 0x1c,
-	IDX_CONTENT_LOCATION	= 0x1d,
-	IDX_CONTENT_RANGE		= 0x1e,
-	IDX_CONTENT_TYPE		= 0x1f,
-	IDX_COOKie				= 0x20,
-	IDX_date				= 0x21,
-	IDX_etag				= 0x22,
-	IDX_EXPECT				= 0x23,
-	IDX_EXPIRES				= 0x24,
-	IDX_FROM				= 0x25,
-	IDX_HOST				= 0x26,
-	IDX_IF_MATCH			= 0x27,
-	IDX_IF_MODIFIED			= 0x28,
-	IDX_IF_NONE_MATCH		= 0x29,
-	IDX_IF_RANGE			= 0x2a,
-	IDX_IF_UNMODIFIED_SINCE = 0x2b,
-	IDX_LAST_MODIFIED		= 0x2c,
-	IDX_LINK				= 0x2d,
-	IDX_LOCATION			= 0x2e,
-	IDX_MAX_FORWARDS		= 0x2f,
-	IDX_PROXY_AUTHENTICATE	= 0x30,
-	IDX_PROXY_AUTHORIZATION	= 0x31,
-	IDX_RANGE				= 0x32,
-	IDX_REFERER				= 0x33,
-	IDX_REFRESH				= 0x34,
-	IDX_RETRY_AFTER			= 0x35,
-	IDX_SERVER				= 0x36,
-	IDX_SET_COOKIE			= 0x37,
-	IDX_STRICT_TRANSPORT_SECURITY = 0x38,
-	IDX_TRANSFER_ENCODING		  = 0x39,
-	IDX_USER_AGENT			= 0x3a,
-	IDX_VARY				= 0x3b,
-	IDX_VIA					= 0x3c,
-	IDX_WWW_AUTHENTICATE	= 0x3d,
-};
-
-HEADER_FIELD static_table[] = {
-	pair(":authority", ""), // index 1 (1-based)
-	pair(":method", "GET"),
-	pair(":method", "POST"),
-	pair(":path", "/"),
-	pair(":path", "/index.html"),
-	pair(":scheme", "http"),
-	pair(":scheme", "https"),
-	pair(":status", "200"),
-	pair(":status", "204"),
-	pair(":status", "206"),
-	pair(":status", "304"),
-	pair(":status", "400"),
-	pair(":status", "404"),
-	pair(":status", "500"),
-	pair("accept-charset", ""),
-	pair("accept-encoding", "gzip, deflate"),
-	pair("accept-language", ""),
-	pair("accept-ranges", ""),
-	pair("accept", ""),
-	pair("access-control-allow-origin", ""),
-	pair("age", ""),
-	pair("allow", ""),
-	pair("authorization", ""),
-	pair("cache-control", ""),
-	pair("content-disposition", ""),
-	pair("content-encoding", ""),
-	pair("content-language", ""),
-	pair("content-length", ""),
-	pair("content-location", ""),
-	pair("content-range", ""),
-	pair("content-type", ""),
-	pair("cookie", ""),
-	pair("date", ""),
-	pair("etag", ""),
-	pair("expect", ""),
-	pair("expires", ""),
-	pair("from", ""),
-	pair("host", ""),
-	pair("if-match", ""),
-	pair("if-modified-since", ""),
-	pair("if-none-match", ""),
-	pair("if-range", ""),
-	pair("if-unmodified-since", ""),
-	pair("last-modified", ""),
-	pair("link", ""),
-	pair("location", ""),
-	pair("max-forwards", ""),
-	pair("proxy-authenticate", ""),
-	pair("proxy-authorization", ""),
-	pair("range", ""),
-	pair("referer", ""),
-	pair("refresh", ""),
-	pair("retry-after", ""),
-	pair("server", ""),
-	pair("set-cookie", ""),
-	pair("strict-transport-security", ""),
-	pair("transfer-encoding", ""),
-	pair("user-agent", ""),
-	pair("vary", ""),
-	pair("via", ""),
-	pair("www-authenticate", ""),
-	
-};
+#define HEX_TO_HF_CODE(hex)		huffman_codes[hex]
+#define HEX_TO_HF_CODE_LEN(hex) huffman_code_len[hex]
 
 NODE *ROOT = NULL;
 
-
-int dynamic_table_add(char *name, char *value){
-	int i = 0;
-	for(i = 0 ; i < dynamic_table_length;i++){
-		if( STRCMP(name, dynamic_table[i].name) ){
-			return HM_RETURN_EXIST_NAME;
-		}
-	}
-	dynamic_table_length++;
-	STRCPY(dynamic_table[i].name, name);
-	STRCPY(dynamic_table[i].value, value);
-	
-	return HM_RETURN_SUCCESS;
-}
-
-int dynamic_table_replace(char *name, char *value){
-	if( STRLEN(value) > MAX_HEADER_VALUE_SIZE ){
-		return HM_RETURN_EXCEED_SIZE;
-	}
-	int i = 0;
-	for(i = 0; i < dynamic_table_length;i++){
-		if( STRCMP(name, dynamic_table[i].name) ){
-			STRCPY(dynamic_table[i].value, value);
-			return HM_RETURN_SUCCESS;
-		}
-	}
-	
-	return HM_RETURN_NOT_FOUND_NAME;
-}
-
-int dynamic_table_delete(char *name){
-	int i = 0;
-	for(i = 0; i < dynamic_table_length;i++){
-		if( STRCMP(name, dynamic_table[i].name) ){
-			STRRESET(dynamic_table[i].name);
-			STRRESET(dynamic_table[i].value);
-			return HM_RETURN_SUCCESS;
-		}
-	}
-	
-	return HM_RETURN_NOT_FOUND_NAME;
-}
-
-int dynamic_table_search(char *name, char *value){
-	int i = 0;
-	for(i = 0; i < dynamic_table_length;i++){
-		if( STRCMP(name, dynamic_table[i].name) ){
-			STRCPY(value, dynamic_table[i].value);
-			return HM_RETURN_SUCCESS;
-		}
-	}
-	
-	return HM_RETURN_NOT_FOUND_NAME;
-}
-
-int hf_search(char *name, char *value){
-	return HM_RETURN_UNIMPLEMENT;
-}
-
-static int get_field_type( unsigned char ch){
-	//Read index header
-	//5f 8b 1d 75 d0 62 0d 26 3d 4c 4d 65 64 : "content-type" = "application/grpc" 
-	ch &= 0xf0;
-	switch( ch ){
-		case INDEX_HEADER_FIELD:
-			break;
-		case LITERAL_HEADER_INCREMENT_INDEXING_FIELD:
-			break;
-		case LITERAL_HEADER_WITHOUT_INDEXING_FIELD:
-			break;
-		case LITERAL_HEADER_NEVER_INDEXING_FIELD:
-			break;
-		case DYNAMIC_HEADER_UPDATE_FIELD:
-			break;
-		default: 
-			return -1;
-
-	}
-	return HM_RETURN_SUCCESS;
-}
-
-
-#define HEX_TO_HF_CODE(hex)		huffman_codes[hex]
-#define HEX_TO_HF_CODE_LEN(hex) huffman_code_len[hex]
-int _hf_string_encode(unsigned char ch, int remain, unsigned char *buff){
-	unsigned char t = 0;
-	int i			= 0;
-	int codes		= HEX_TO_HF_CODE(ch);
-	int nbits		= HEX_TO_HF_CODE_LEN(ch);
-	printf("'%c'|codes(%d)|len(%d)\n", ch, codes, nbits );
-	for(;;){
-		if( remain > nbits){
-			t = (unsigned char)(codes << (remain-nbits));
-			buff[i++] |= t;
-			return remain-nbits;
-		}else{
-			t = (unsigned char )(codes >> (nbits-remain));
-			buff[i++] |= t;
-			nbits -= remain;
-			remain = 8;
-		}
-		buff[i] = 0;
-		if(nbits == 0) return remain;
-	}
-
-}
-
 static NODE * node_create(){
 	NODE *nnode = (NODE*)malloc(1*sizeof(NODE));
-	//nnode->children = (NODE*)malloc(256 * sizeof(struct node *));
 	memset(nnode, 0, sizeof(NODE));
 	return nnode;
 }
@@ -292,21 +49,36 @@ static int _hf_add_node(unsigned char sym, int code, int code_len){
 	return 0;
 }
 
-int hf_init_root(){
-	int i = 0;
-	ROOT = node_create();
-	
+int hf_init(){
+	int i   = 0;
+	ROOT    = node_create();
 	for(i = 0; i < 256; i++){
 		_hf_add_node(i, huffman_codes[i], huffman_code_len[i]);
 	}
-
 	return 0;
 }
 
+static int _hf_string_encode(unsigned char ch, int remain, unsigned char *buff){
+	unsigned char t = 0;
+	int i			= 0;
+	int codes		= HEX_TO_HF_CODE(ch);
+	int nbits		= HEX_TO_HF_CODE_LEN(ch);
+	printf("'%c'|codes(%d)|len(%d)\n", ch, codes, nbits );
+	for(;;){
+		if( remain > nbits){
+			t = (unsigned char)(codes << (remain-nbits));
+			buff[i++] |= t;
+			return remain-nbits;
+		}else{
+			t = (unsigned char )(codes >> (nbits-remain));
+			buff[i++] |= t;
+			nbits -= remain;
+			remain = 8;
+		}
+		buff[i] = 0;
+		if(nbits == 0) return remain;
+	}
 
-
-int hf_decode_print(NODE *node){
-	return HM_RETURN_UNIMPLEMENT;
 }
 
 int hf_string_encode(char *buff_in, int size, int prefix, unsigned char *buff_out, int *size_out){
@@ -342,42 +114,7 @@ int hf_string_encode(char *buff_in, int size, int prefix, unsigned char *buff_ou
 	return 0;
 }
 
-int _hf_string_decode(unsigned char *enc_in, int size, int prefix, char *buff_out, int *size_out);
-int hf_string_decode(unsigned char *enc_in, int size, int prefix, char *buff_out, int *size_out){
-
-}
-
-int _hf_encode(unsigned int enc_binary, int nprefix,unsigned char *buff){
-}
-
-int hf_encode(unsigned int enc_binary, int nprefix, unsigned char *buff){
-	int i = 0;
-	unsigned int ch	    = enc_binary;
-	unsigned int ch2    = 0;
-	unsigned int prefix = (1 << nprefix) - 1;
-
-	printf("ch : %u\n", ch);
-	
-	if( ch < prefix  && (ch < 0xff) ){
-		buff[i++] = ch & prefix;
-		printf("encoded	as: %u\n", prefix );
-	}else{
-		buff[i++] = prefix;
-		printf("encoded	as: %u\n", prefix );
-		ch -= prefix;
-		while(ch > 128)
-		{
-			ch2 = (ch % 128);
-			ch2 += 128;
-			buff[i++] = ch2;
-			ch = ch/128;
-		}
-		buff[i++] = ch;
-	}
-	return HM_RETURN_UNIMPLEMENT;
-}
-
-int hf_decode_string(unsigned char *enc,int enc_sz, char *out_buff,int out_sz){
+int hf_string_decode(unsigned char *enc, int enc_sz, char *out_buff, int out_sz){
 	NODE *n			  = ROOT;
 	unsigned int cur  = 0;
 	unsigned char b	  = 0;
@@ -425,8 +162,34 @@ int hf_decode_string(unsigned char *enc,int enc_sz, char *out_buff,int out_sz){
 	return at;
 }
 
-int hf_decode(char *enc_buff, int nprefix , char *dec_buff){
+int hf_interger_encode(unsigned int enc_binary, int nprefix, unsigned char *buff){
+	int i = 0;
+	unsigned int ch	    = enc_binary;
+	unsigned int ch2    = 0;
+	unsigned int prefix = (1 << nprefix) - 1;
 
+	printf("ch : %u\n", ch);
+	
+	if( ch < prefix  && (ch < 0xff) ){
+		buff[i++] = ch & prefix;
+		printf("encoded	as: %u\n", prefix );
+	}else{
+		buff[i++] = prefix;
+		printf("encoded	as: %u\n", prefix );
+		ch -= prefix;
+		while(ch > 128)
+		{
+			ch2 = (ch % 128);
+			ch2 += 128;
+			buff[i++] = ch2;
+			ch = ch/128;
+		}
+		buff[i++] = ch;
+	}
+	return HM_RETURN_SUCCESS;
+}
+
+int hf_integer_decode(char *enc_buff, int nprefix , char *dec_buff){
 	int i				= 0;
 	int j				= 0;
 	unsigned int M		= 0;
@@ -449,7 +212,7 @@ int hf_decode(char *enc_buff, int nprefix , char *dec_buff){
 		printf("decode ch : %u\n", ch);
 		dec_buff[j] = ch;
 	}
-	return HM_RETURN_UNIMPLEMENT;
+	return HM_RETURN_SUCCESS;
 }
 
 void hf_print_hex(unsigned char *buff, int size){
@@ -462,46 +225,6 @@ void hf_print_hex(unsigned char *buff, int size){
 		printf("%c ", hex[(ch&0x0f)]);
 	}
 	printf("\n");
-}
-
-int test_hf_string_encode_3bytes(){
-	char b[10];
-	b[0] = 0;
-	b[1] = 0;
-	int r = _hf_string_encode('a', 8, &b[0]);
-	r = _hf_string_encode('p', r, &b[0]);
-	r = _hf_string_encode('p', r, &b[1]);
-	if( r ==7 && b[0] == 0x1d && b[1] == 0x75) return 1;
-	printf("expected 7:%d , %d:%d, %d:%d\n", r,0x1d,b[0], 0x75, b[1]);
-	return 0;
-}
-
-int test_hf_string_encode_2bytes(){
-	char b[10];
-	b[0] = 0;
-	int r = _hf_string_encode('a', 8, &b[0]);
-	r = _hf_string_encode('p', r, &b[0]);
-	if( r ==5 && b[0] == 0x1d) return 1;
-	printf("expected 3:%d , %d:%d\n", r,0x18,b[0]);
-	return 0;
-}
-
-int test_hf_string_encode_1byte(){
-	char b[10];
-	b[0] = 0;
-	int r = _hf_string_encode('a', 8, b);
-	if( r ==3 && b[0] == 0x18) return 1;
-	printf("expected 3:%d , %d:%d\n", r,0x18,b[0]);
-	return 0;
-}
-
-void test_all(){
-	printf("test_hf_string_encode_1byte():%s\n",
-			test_hf_string_encode_1byte()?"PASS":"FAILED");
-	printf("test_hf_string_encode_2bytes():%s\n",
-			test_hf_string_encode_2bytes()?"PASS":"FAILED");
-	printf("test_hf_string_encode_3bytes():%s\n",
-			test_hf_string_encode_3bytes()?"PASS":"FAILED");
 }
 
 
--- huffman/test_huffman.c
+++ huffman/test_huffman.c
@@ -5,42 +5,56 @@
 #include "tables.h"
 #include "huffman.h"
 
+static int test_hf_string_encode_3bytes(){
+	char b[10];
+	b[0] = 0;
+	b[1] = 0;
+	int r = _hf_string_encode('a', 8, &b[0]);
+	r = _hf_string_encode('p', r, &b[0]);
+	r = _hf_string_encode('p', r, &b[1]);
+	if( r ==7 && b[0] == 0x1d && b[1] == 0x75) return 1;
+	printf("expected 7:%d , %d:%d, %d:%d\n", r,0x1d,b[0], 0x75, b[1]);
+	return 0;
+}
+
+static int test_hf_string_encode_2bytes(){
+	char b[10];
+	b[0] = 0;
+	int r = _hf_string_encode('a', 8, &b[0]);
+	r = _hf_string_encode('p', r, &b[0]);
+	if( r ==5 && b[0] == 0x1d) return 1;
+	printf("expected 3:%d , %d:%d\n", r,0x18,b[0]);
+	return 0;
+}
+
+static int test_hf_string_encode_1byte(){
+	char b[10];
+	b[0] = 0;
+	int r = _hf_string_encode('a', 8, b);
+	if( r ==3 && b[0] == 0x18) return 1;
+	printf("expected 3:%d , %d:%d\n", r,0x18,b[0]);
+	return 0;
+}
+
+void test_all(){
+	printf("test_hf_string_encode_1byte():%s\n",
+			test_hf_string_encode_1byte()?"PASS":"FAILED");
+	printf("test_hf_string_encode_2bytes():%s\n",
+			test_hf_string_encode_2bytes()?"PASS":"FAILED");
+	printf("test_hf_string_encode_3bytes():%s\n",
+			test_hf_string_encode_3bytes()?"PASS":"FAILED");
+}
+
 int main(){
-	printf("%d\n", huffman_codes[0]);
-	printf("%d\n", huffman_code_len[0]);
-
-	char name[MAX_HEADER_NAME_SIZE];
-	char value[MAX_HEADER_VALUE_SIZE];
-
-	dynamic_table_add("path", "toro");
-	dynamic_table_replace("path", "root");
-	dynamic_table_search("path", value);
-	printf("fund path: %s\n", value);
 
 	unsigned char buff[10];
 	memset(buff, 0 ,10);
-	hf_encode((unsigned int)1337, 5, buff);
+	hf_integer_encode((unsigned int)1337, 5, buff);
 	printf("test Dedode 1337: %u:%u:%u:%u:%u\n", buff[0],buff[1],buff[2],buff[3],buff[4]);
-
-	memset(buff, 0 ,10);
-	hf_encode((unsigned int)128, 5, buff);
-	printf("test Dedode 128 : %u:%u:%u\n", buff[0], buff[1], buff[2]);
-		
-	memset(buff, 0 ,10);
-	hf_encode((unsigned int)16628, 5, buff);
-	printf("test Dedode 257 : %u:%u:%u\n", buff[0], buff[1], buff[2]);
-	memset(buff, 0 ,10);
-	hf_encode((unsigned char)'b', 5, buff);
-	printf("test Dedode 'b': %u:%u:%u:%u:%u\n", buff[0],buff[1],buff[2],buff[3],buff[4]);
-	
-
-	memset(buff, 0 ,10);
-	hf_encode((unsigned int)'p', 5, buff);
-	printf("test Dedode 'p': %u:%u:%u:%u:%u\n", buff[0],buff[1],buff[2],buff[3],buff[4]);
 
 	char dec_buff[10];
 	memset(dec_buff, 0 ,10);
-	hf_decode(buff, 5, dec_buff);
+	hf_integer_decode(buff, 5, dec_buff);
 	printf("test Dedode 'p': %u:%u:%u:%u:%u\n", dec_buff[0],dec_buff[1],dec_buff[2],dec_buff[3],dec_buff[4]);
 	
 
@@ -75,11 +89,8 @@ int main(){
 	
 	test_all();
 
-	hf_init_root();
-	hf_decode_print(ROOT);
-	
-	
-	char out_buff[128];
+	hf_init();
+    char out_buff[128];
 	int size = hf_decode_string(bout, sz , out_buff, 128);
 	if( strncmp(out_buff, bin, size) == 0){
 		printf("hf_decode_string():PASS\n");
@@ -88,10 +99,6 @@ int main(){
 		hf_print_hex(out_buff, size);
 	}
 	
-	
 	return 0;
-
-
-
 }
 
