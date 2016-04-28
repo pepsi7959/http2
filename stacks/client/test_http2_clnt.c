a151198fcfd4eca54d82199d968180153ae777c7
pepsi7959 <pepsi_kmutnb@windowslive.com>
2016-04-28 19:31:15 +0700

    updated test_http2_clnt

--- stacks/client/test_http2_clnt.c
+++ stacks/client/test_http2_clnt.c
@@ -1,4 +1,4 @@
-#include "http_clnt.h"
+#include "http2_clnt.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -10,17 +10,17 @@ int main(){
 	HCSESSION *sess = NULL;
 	char error[1024];
     char  buff[1024];
-	HTTP_CLNT_t *hc = HTTP_CLNT_init("dmcd",1024,1024);
+	HTTP2_CLNT_t *hc = HTTP2_CLNT_init("dmcd",1024,1024);
 	printf("hc group : %s\n", hc->group);
-	HTTP_CLNT_add_host(hc, "10.252.192.31", 20600, 0,1);
-	HTTP_CLNT_sess_open(hc, &sess, error);
+	HTTP2_CLNT_add_host(hc, "10.252.192.31", 20600, 0,1);
+	HTTP2_CLNT_sess_open(hc, &sess, error);
 	fd_set rfds;
 
 	struct timeval tv;
     int retval;
     sleep(2);
     sess->w_buffer->len = sprintf(sess->w_buffer->data, 
-    "POST / HTTP/1.1\n"
+    "POST / HTTP2/1.1\n"
     "Content-Type: text/xml\n"
     "Host localhost:20600\n"
     "Content-Length: 79"
@@ -29,7 +29,7 @@ int main(){
     "<EquinoxCommand name=\"getstats\" cid=\"3989\"/>\n"
     "</EquinoxMessage>\n");
     
-    HTTP_CLNT_sess_write(hc, sess, NULL, error);
+    HTTP2_CLNT_sess_write(hc, sess, NULL, error);
     
     for(;;){
         /* Watch stdin (fd 0) to see when it has input. */
@@ -52,21 +52,21 @@ int main(){
            printf("No data within five seconds.\n");
          
          if( FD_ISSET(sess->sock, &rfds) ){
-             if( HTTP_CLNT_sess_read(hc, sess, error) == HTTP_CLNT_RET_OK ){
+             if( HTTP2_CLNT_sess_read(hc, sess, error) == HTTP2_CLNT_RET_OK ){
                  int len = sess->r_buffer->len;
                  printf("data len : %d", len);
                  memcpy(buff, sess->r_buffer->data, len);
                  buff[len] = 0;
                  printf(" data : %s\n", buff);
                  
-                 if( (retval = HTTP_CLNT_sess_decode(hc, sess, error)) == HTTP_CLNT_RET_DATA_AVAILABLE){
+                 if( (retval = HTTP2_CLNT_sess_decode(hc, sess, error)) == HTTP2_CLNT_RET_DATA_AVAILABLE){
                      printf(" header len: %d\n", sess->header_length);
                      printf("--------------=-------------------");
                      printf("[ \n%s\n ]", sess->r_buffer->data);
                      printf("--------------=-------------------");
                      printf(" body len: %d\n", sess->body_length);
                      printf("[ \n%s\n ]", &sess->r_buffer->data[sess->header_length]);
-                     HTTP_CLNT_sess_close(hc, sess->no, NULL, error);
+                     HTTP2_CLNT_sess_close(hc, sess->no, NULL, error);
                      exit(0);
                  }else {
                      printf(" status :: %d\n", retval);
@@ -77,7 +77,7 @@ int main(){
     }
     
     printf("recv : %s\n", error);
-	HTTP_CLNT_sess_close(hc, sess->no, NULL, error);
+	HTTP2_CLNT_sess_close(hc, sess->no, NULL, error);
 	
 
 	return 0;
