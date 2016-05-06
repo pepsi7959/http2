1b25d2cc265545f0721c4459aa942219df56762d
pepsi7959 <pepsi_kmutnb@windowslive.com>
2016-05-06 17:23:22 +0700

    Add common.h test_common.c

---
+++ common/common.h
@@ -0,0 +1,27 @@
+#ifndef _HTTP2_COMMON_H
+ #define _HTTP2_COMMON_H
+
+#define READBYTE(ch, cur, _sz, v) do{       \
+    v               = 0;                    \
+    int sz          = 0;					\
+    unsigned int vv = 0;                    \
+    unsigned char *pch = ch+cur;            \
+    while(sz < _sz){                        \
+        vv = (unsigned int)*pch & 0xff;     \
+        v = (sz > 0)?v << 8:v;                \
+        v = (v|vv);				            \
+        pch++;                              \
+        sz++;                               \
+    }                                       \
+}while(0)
+
+ 
+struct _buffer_t{
+    struct HTTP2_buffer_t *prev;
+    struct HTTP2_buffer_t *next;
+    int size;
+    int len;
+    int cur;
+    unsigned char data[1];
+};
+#endif
