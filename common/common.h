4c294f6f7622f3dde69134933e9315180cda1bf4
pepsi7959 <pepsi_kmutnb@windowslive.com>
2016-05-21 15:46:56 +0700

    Revised souce code format

--- common/common.h
+++ common/common.h
@@ -3,19 +3,18 @@
 
 #define READBYTE(ch, cur, _sz, v) do{       \
     v               = 0;                    \
-    int sz          = 0;					\
+    int sz          = 0;                    \
     unsigned int vv = 0;                    \
     unsigned char *pch = ch+cur;            \
     while(sz < _sz){                        \
         vv = (unsigned int)*pch & 0xff;     \
-        v = (sz > 0)?v << 8:v;                \
-        v = (v|vv);				            \
+        v = (sz > 0)?v << 8:v;              \
+        v = (v|vv);                         \
         pch++;                              \
         sz++;                               \
     }                                       \
 }while(0)
 
- 
 struct _buffer_t{
     struct _buffer_t *prev;
     struct _buffer_t *next;
