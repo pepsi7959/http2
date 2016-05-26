3ed73520345db0d8e556b0737c302920094c4d81
pepsi7959 <pepsi_kmutnb@windowslive.com>
2016-05-26 16:05:38 +0700

    Added ALLOCATE_BUFFER()

--- common/common.h
+++ common/common.h
@@ -1,6 +1,8 @@
 #ifndef _HTTP2_COMMON_H
  #define _HTTP2_COMMON_H
 
+ #define DATA_BUFFER    4096
+ 
 #define READBYTE(ch, cur, _sz, v) do{       \
     v               = 0;                    \
     int sz          = 0;                    \
@@ -13,6 +15,29 @@
         pch++;                              \
         sz++;                               \
     }                                       \
+}while(0)
+
+
+#define ALLOCATE_BUFFER(_d, _l) do{         \
+    int __l = _l;                           \
+	if (_d == NULL)                         \
+	{                                       \
+		_d = malloc(sizeof(*_d) + __l);     \
+		if (_d == NULL) break;              \
+		_d->data[0] = 0;                    \
+		_d->size    = __l;                  \
+		_d->len     = 0;                    \
+	}                                       \
+	else if ((_d->len + __l) > _d->size)    \
+	{                                       \
+		typeof(*_d) *x;                     \
+		__l += _d->len;                     \
+		x = realloc(_d, sizeof(*_d) + __l); \
+		if (x == NULL) break;               \
+		x->size = __l;                      \
+		_d = x;                             \
+		_d->data[_d->len] = 0;              \
+	}                                       \
 }while(0)
 
 struct _buffer_t{
